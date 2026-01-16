2accd144af92f7b3002d57a1c73feedcba318973
The Sophisticated Programmer <ahmad.5512.797@gmail.com>
2026-01-16 17:07:14 +0000

    Enhance comments in day1.py for clarity
    Added comments to explain the for loop and range.

--- day1.py
+++ day1.py
@@ -1,5 +1,6 @@
 for i in range(1, 101): 
     print(i)
     # for i - what i is, what will we make i do. in range - we will give i a range. (1, 101): - we choose the range of i.
-
+    # observations: ranges are closed intervals.
+    # what have I learnt: for - giving a variable something & in range - gives a variable a range.
 
