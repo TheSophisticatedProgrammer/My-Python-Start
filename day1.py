ecdb993a940a8c284292f8b23c0c376be682c591
The Sophisticated Programmer <ahmad.5512.797@gmail.com>
2026-01-16 17:05:50 +0000

    Fix comments formatting in day1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
 for i in range(1, 101): 
     print(i)
-    # for i - what i is, what will we make i do. in range - we will give i a range. (1, 101): - we choose the range of i
+    # for i - what i is, what will we make i do. in range - we will give i a range. (1, 101): - we choose the range of i.
+
+
